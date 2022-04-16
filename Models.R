0a4842cabd37006be329ccbef11ddd74418d591c
Kathleen Cimino <kathleenecimino@gmail.com>
2022-04-16 12:25:26 -0400

    Clean code so Scratch can source just one script

--- Models.R
+++ Models.R
@@ -1,11 +1,14 @@
-source("CLeaning.R")
+source(str_c(ps::ps_cwd(),"/Cleaning.R"))
+source(str_c(ps::ps_cwd(),"/Functions.R"))
 source("Functions.R")
 
 #Modelling
 
 dfmodel <- casseroledf2(casseroles[1])
-rbind(dfmodel,dfbroc1)
 
 for (i in 2:10){
   dftemp <- casseroledf2(casseroles[i])
   dfmodel <- rbind(dfmodel,dftemp)}
+
+
+rm(df1,df,dftemp,i)
